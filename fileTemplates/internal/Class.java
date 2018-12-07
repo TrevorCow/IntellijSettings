29ea2479aaf5e85c00834966f5748e805ef3d0e2
Trevor Rosenstrom <TrevorCow@users.noreply.github.com>
2018-12-06 20:22:11 -0800

    IC-2018.2.4 <Trevor@TRazerV2 Create Groovy Class.groovy, ide.general.xml, Trevor.xml, git.xml, project.default.xml, Default copy.xml, other.xml, Default.xml, applicationLibraries.xml, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, colors.scheme.xml, CSS143.xml, github_settings.xml, Java.xml, scala.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, editor.codeinsight.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+/** 
+ * @author Trevor Rosenstrom 
+ * Section:  143A 
+ * Assignment:  ${PROJECT_NAME}
+ */
+
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public class ${NAME} {
+}
